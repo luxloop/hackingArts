ed9cbdda542091ac288ea369be4bdbea8f1797d4
Ivaylo Getov <ivaylo.getov@gmail.com>
2014-10-04 22:28:25 -0400

    rebuilding...

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,17 +2,98 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofSetVerticalSync(true);
+	ofSetFrameRate(60);
+    ofBackground(0);
+    
+    for(int i=0; i<N_VIDEO_PLAYERS_1; i++) {
+        videoPlayers1.push_back(new ofxAVFVideoPlayer());
+    }
+    videoPlayers1[0]->loadMovie("videos/fingers.mov");
+    videoPlayers1[1]->loadMovie("videos/fingers2.mov");
+    for(auto p : videoPlayers1) {
+        //p->play();
+        p->setLoopState(OF_LOOP_NORMAL);
+    }
+    
+	ofSetLogLevel(OF_LOG_VERBOSE);
+    dmode = A;
+    eyeTest.loadMovie("eyeroll.mov");
+    eyeTest.stop();
+    meters = 0.0;
+    feet = 0.0;
+    world = ofVec3f(0,1,1);
+    float angle = 0.0;
+    
+    
+    gui.setup(); // most of the time you don't need a name
+    
+    gui.add(minBlob.setup("Min Blob Size", 10000, 5, 20000));
+    gui.add(maxBlob.setup("Max Blob Size", 300000, 5, 800000));
+	gui.add(bgSubThreshold.setup("BG Sub Threshold", 80, 1, 255));
+    
+	//kinect.setRegistration(true);  // enable depth->video image calibration
+    
+	kinect.init();
+	//kinect.init(true); // shows infrared instead of RGB video image
+	//kinect.init(false, false); // disable video image (faster fps)
+    //kinect.setDepthClipping(500,4000);
+	kinect.open();
+	
+    /*
+     // print the intrinsic IR sensor values
+     if(kinect.isConnected()) {
+     ofLogNotice() << "sensor-emitter dist: " << kinect.getSensorEmitterDistance() << "cm";
+     ofLogNotice() << "sensor-camera dist:  " << kinect.getSensorCameraDistance() << "cm";
+     ofLogNotice() << "zero plane pixel size: " << kinect.getZeroPlanePixelSize() << "mm";
+     ofLogNotice() << "zero plane dist: " << kinect.getZeroPlaneDistance() << "mm";
+     }
+     */
+	
+#ifdef USE_TWO_KINECTS
+	kinect2.init();
+	kinect2.open();
+#endif
+	
+	colorImg.allocate(kinect.width, kinect.height);
+	grayImage.allocate(kinect.width, kinect.height);
+	grayThreshNear.allocate(kinect.width, kinect.height);
+	grayThreshFar.allocate(kinect.width, kinect.height);
+    
+    
+	grayCamImage.allocate(kinect.width, kinect.height);
+	grayBg.allocate(kinect.width, kinect.height);
+	grayDiff.allocate(kinect.width, kinect.height);
+    
+	bLearnBakground = true;
+    //bgSubThreshold = 30;
+	
+	nearThreshold = 230;
+	farThreshold = 70;
+	
+	
+	// zero the tilt on startup
+	//kinectAngle = 0;
+	//kinect.setCameraTiltAngle(kinectAngle);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    for(auto p : videoPlayers1) {
+        p->update();
+    }
+    if (videoPlayers1[0]->isLoaded() && videoPlayers1[1]->isLoaded()) {
+        videoPlayers1[0]->play();
+        videoPlayers1[1]->play();
+    }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    //video.draw(0, 0);
+    videoPlayers1[0]->draw(0, 0);
+    videoPlayers1[1]->draw(ofGetWidth()/2, 0);
+    
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,22 +1,76 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxKinect.h"
+#include "ofxGui.h"
+#include "ofxAVFVideoPlayer.h"
 
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+public:
+    void setup();
+    void update();
+    void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    
+    ofxIntSlider minBlob;
+    ofxIntSlider maxBlob;
+    ofxIntSlider bgSubThreshold;
+    
+	ofxPanel gui;
+    
+    ofxKinect kinect;
+    
+#ifdef USE_TWO_KINECTS
+	ofxKinect kinect2;
+#endif
+	
+	ofxCvColorImage colorImg;
+	
+	ofxCvGrayscaleImage grayImage; // grayscale depth image
+	ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
+	ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
+    
+    
+    //ofxCvColorImage			colorCamImg;
+    ofxCvGrayscaleImage 	grayCamImage;
+    ofxCvGrayscaleImage 	grayBg;
+    ofxCvGrayscaleImage 	grayDiff;
+    
+    //int bgSubThreshold;
+    bool bLearnBakground;
+	
+	ofxCvContourFinder contourFinder;
+	
+	bool bThreshWithOpenCV;
+	
+	int nearThreshold;
+	int farThreshold;
+	
+	int kinectAngle;
+    
+    enum displayModes {A, B, C};
+    displayModes dmode;
+    
+    ofVideoPlayer eyeTest;
+    float meters;
+    float feet;
+    ofVec3f world;
+    float angle;
+    
+    
+    
+    std::vector<ofxAVFVideoPlayer *> videoPlayers1;
+    static const int N_VIDEO_PLAYERS_1 = 2;
+    	
 };
